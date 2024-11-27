ab764d8fabf677ee248a7b68d929cec59391813f
joseJp <jose.han@asa.edu.py>
2024-11-27 10:18:42 -0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ let NIckle = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . e e e e e e . . . . . . 
     . . . . e 1 e e 1 e . . . . . . 
-    . . . . e e e e e e e . . . . . 
+    . . . . e e e e e e . . . . . . 
     . . . . e d d d d e . . . . . . 
     . . . . e e e e e e . . . . . . 
     . . . . . . e e . . . . . . . . 
@@ -12,8 +12,9 @@ let NIckle = sprites.create(img`
     . . . . . e e e e . . . . . . . 
     . . . . . e e e e . . . . . . . 
     . . . . . 5 5 5 5 . . . . . . . 
-    . . . e 5 5 5 5 5 5 . . . . . . 
+    . . . e 5 5 3 3 5 5 . . . . . . 
     . . e e . . . . . e e . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(NIckle)
+scroller.scrollBackgroundWithCamera(scroller.CameraScrollMode.OnlyHorizontal)
