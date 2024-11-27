6790c7e3ec74c04a75d325ec503e980c59b54aa4
joseJp <jose.han@asa.edu.py>
2024-11-27 10:09:03 -0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . e e e e e e . . . . . . 
+    . . . . e d e e d e . . . . . . 
+    . . . . e e e e e e e . . . . . 
+    . . . . e d d d d e . . . . . . 
+    . . . . e e e e e e . . . . . . 
+    . . . . . . e . . . . . . . . . 
+    . . . . . . e . . . . . . . . . 
+    . . . . . e e e e e e e . . . . 
+    e e e e e . e e . . . . . . . . 
+    . . . . . . . e . . . . . . . . 
+    . . . . . e e e . . . . . . . . 
+    . . . e e e . e e e . . . . . . 
+    . . . e . . . . . e e . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
