40f4d13f65f949633346c6cfda9dc70de6bcc9dd
Ferni-45 <fernando.garcia@asa.edu.py>
2024-11-27 10:13:47 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
-let mySprite = sprites.create(img`
+let NIckle = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . e e e e e e . . . . . . 
-    . . . . e d e e d e . . . . . . 
+    . . . . e 1 e e 1 e . . . . . . 
     . . . . e e e e e e e . . . . . 
     . . . . e d d d d e . . . . . . 
     . . . . e e e e e e . . . . . . 
-    . . . . . . e . . . . . . . . . 
-    . . . . . . e . . . . . . . . . 
-    . . . . . e e e e e e e . . . . 
-    e e e e e . e e . . . . . . . . 
-    . . . . . . . e . . . . . . . . 
-    . . . . . e e e . . . . . . . . 
-    . . . e e e . e e e . . . . . . 
-    . . . e . . . . . e e . . . . . 
+    . . . . . . e e . . . . . . . . 
+    . . . . . . e e . . . . . . . . 
+    . . e e e e e e e e e e . . . . 
+    . . . . . e e e e . . . . . . . 
+    . . . . . e e e e . . . . . . . 
+    . . . . . 5 5 5 5 . . . . . . . 
+    . . . e 5 5 5 5 5 5 . . . . . . 
+    . . e e . . . . . e e . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
+controller.moveSprite(NIckle)
